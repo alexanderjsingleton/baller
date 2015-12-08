9c44ffea1b7d8c202f780de3f46f4d66a8d0a523
Alex Singleton <alexanderjsingleton@gmail.com>
2015-12-08 10:56:32 -0600

    added ball behavior: 'Ball me, Double, ball me'

---
+++ JOC/Ball.java
@@ -0,0 +1,36 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Ball here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ball extends Actor
+{
+    /**
+     * Act - do whatever the Ball wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(4);
+        if(atWorldEdge()) {
+            turn(153);
+        }
+    }
+    
+    /**
+     * Test if we are close to one of the edges of the world. Return true is we are.
+     */
+    public boolean atWorldEdge()
+    {
+        if(getX() < 10 || getX() > getWorld().getWidth() - 10)
+            return true;
+        if(getY() < 10 || getY() > getWorld().getHeight() - 10)
+            return true;
+        else
+            return false;
+    }
+
+}
